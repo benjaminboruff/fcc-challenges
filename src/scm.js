7446e1b2ffe321905b9f72a8fd31ac33295125d6
benjaminboruff <ben@boruff.me>
2017-01-02 12:02:38 -0500

    new set of challenges

--- src/scm.js
+++ src/scm.js
@@ -1,6 +1,8 @@
 /* jshint esversion: 6 */
 function smallestCommons(arr) {
   let min, max;
+  let allNums = [];
+
   if (arr[0] < arr[1]) {
     min = arr[0];
     max = arr[1];
@@ -8,17 +10,30 @@ function smallestCommons(arr) {
     min = arr[1];
     max = arr[0];
   }
-  let gcd = ((a, b) => {
-    while(b !== 0) {
-      let t = b;
-      b = a % b;
-      a = t;
-    }
-    return a;
-  })(min, max);
 
-  let lcm = (min / gcd) * max;
-  return lcm;
+  for(let i = min; i <= max; i++) {
+    allNums.push(i);
+  }
+
+  console.log(allNums);
+
+
+
+  let smallestC = lcm(allNums[1], allNums[2]);
+  return smallestC;
+}
+
+function lcm(a,b) {
+  let gcd = ((x, y) => {
+    while(y !== 0) {
+      let t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  })(a, b);
+
+  return (a * b) / gcd;
 }
 
 export default smallestCommons;
