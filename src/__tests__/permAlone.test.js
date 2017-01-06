112f4638c9072518aff1b2148b5038a874c7b16e
benjaminboruff <ben@boruff.me>
2017-01-06 11:52:28 -0500

    recursive permutations

--- src/__tests__/permAlone.test.js
+++ src/__tests__/permAlone.test.js
@@ -1,63 +1,6 @@
 import {
-    permAlone,
-    swapTwo,
-    swapSet
+    permAlone
 } from '../permAlone';
-describe('***** swapTwo *****', () => {
-    it('swapTwo("aab", 0, 1) should return a string', () => {
-        expect(typeof swapTwo("aab", 0, 1)).toBe('string');
-    });
-
-    it('swapTwo("aab", 0, 1) should return "aab"', () => {
-        expect(swapTwo("aab", 0, 1)).toBe('aab');
-    });
-
-    it('swapTwo("aab", 1, 2) should return "aba"', () => {
-        expect(swapTwo("aab", 0, 1)).toBe('aab');
-    });
-
-    it('swapTwo("aab", 0, 2) should return "baa"', () => {
-        expect(swapTwo("aab", 0, 2)).toBe('baa');
-    });
-
-    it('swapTwo("aabb", 0, 1) should return "aabb"', () => {
-        expect(swapTwo("aabb", 0, 1)).toBe('aabb');
-    });
-
-    it('swapTwo("aabb", 1, 2) should return "abab"', () => {
-        expect(swapTwo("aabb", 1, 2)).toBe('abab');
-    });
-
-    it('swapTwo("abab", 2, 3) should return "abba"', () => {
-        expect(swapTwo("abab", 2, 3)).toBe('abba');
-    });
-
-    it('swapTwo("bbaa", 2, 3) should return "bbaa"', () => {
-        expect(swapTwo("bbaa", 2, 3)).toBe('bbaa');
-    });
-});
-
-describe('***** swapSet *****', () => {
-  it('swapSet(aab) ahould return an array', () => {
-    expect(Array.isArray(swapSet("aab"))).toBe(true);
-  });
-
-  it('swapSet(aab) ahould return ["aab", "aba", "aab"]', () => {
-    expect(swapSet("aab")).toEqual(["aab", "aba", "aab"]);
-  });
-
-  it('swapSet(aabb) ahould return ["aabb", "abab", "abba", "abab"]', () => {
-    expect(swapSet("aabb")).toEqual(["aabb", "abab", "abba", "abab"]);
-  });
-
-  it('swapSet(abba) ahould return ["baba", "bbaa", "bbaa", "bbaa"]', () => {
-    expect(swapSet("abba")).toEqual(["baba", "bbaa", "bbaa", "bbaa"]);
-  });
-
-  it('swapSet(abab) ahould return ["baab", "baab", "baba", "baba"]', () => {
-    expect(swapSet("abab")).toEqual(["baab", "baab", "baba", "baab"]);
-  });
-});
 
 describe('***** permaAlone *****', () => {
     it('permAlone("aab") should return a number', () => {
@@ -80,23 +23,23 @@ describe('***** permaAlone *****', () => {
         expect(permAlone("abcdefa")).toBe(3600);
     });
 
-    xit('permAlone("abfdefa") should return 2640', () => {
+    it('permAlone("abfdefa") should return 2640', () => {
         expect(permAlone("abfdefa")).toBe(2640);
     });
 
-    xit('permAlone("zzzzzzzz") should return 0', () => {
+    it('permAlone("zzzzzzzz") should return 0', () => {
         expect(permAlone("zzzzzzzz")).toBe(0);
     });
 
-    xit('permAlone("a") should return 1', () => {
+    it('permAlone("a") should return 1', () => {
         expect(permAlone("a")).toBe(1);
     });
 
-    xit('permAlone("aaab") should return 0', () => {
+    it('permAlone("aaab") should return 0', () => {
         expect(permAlone("aaab")).toBe(0);
     });
 
-    xit('permAlone("aaabb") should return 12', () => {
+    it('permAlone("aaabb") should return 12', () => {
         expect(permAlone("aaabb")).toBe(12);
     });
 });
--- src/permAlone.js
+++ src/permAlone.js
@@ -1,81 +1,35 @@
 /* jshint esversion: 6 */
-var seed = true;
-
 function permAlone(str) {
-    let numSwaps = factorial(str.length) / 2;
-    let list = [];
-    let noRepeats = 0;
+    //Enclosed data to be used by the internal recursive function permutate():
+    let permutations = [], //generated permutations stored here
+        nextWord = [], //next word builds up in here
+        chars = [], //collection for each recursion level
+        noRepeats = 0;
 
-    console.log(numSwaps);
-    for (let i = 0; i < numSwaps / str.length; i++) {
-        //let tmpStr = str;
-        console.log("*****SET*****: " + i);
-        let tmpArr = swapSet(str);
-        console.log(tmpArr[tmpArr.length - 1]);
-        list.push(tmpArr);
-        //swapSet(tmpArr[tmpArr.length - 1]);
-        str = tmpArr[tmpArr.length - 1];
-    }
+    //split words or numbers into an array of characters
+    chars = str.split('');
+    permutate(chars);
 
-    console.log(list);
-    let reduced = list.reduce((a, b) => {
-        return a.concat(b);
-    }, []);
+     // only allow strings with non-repeating chars
     let filter = /(.)\1/g;
-    for (let i = 0; i < reduced.length; i++) {
-        noRepeats += reduced[i].match(filter) === null ? 1 : 0;
+    for (let i = 0; i < permutations.length; i++) {
+        noRepeats += permutations[i].match(filter) === null ? 1 : 0;
     }
-    //console.log(reduced);
-    console.log(noRepeats * 2);
-    return noRepeats * 2;
-}
+    console.log(noRepeats);
+    return noRepeats;
 
-function swapSet(str) {
-    let arr = [];
-    if (seed) {
-        arr.push(str);
-        seed = false;
-    }
-    for (let i = 1; i < str.length; i++) {
-        str = swapTwo(str, i - 1, i);
-        arr.push(str);
-    }
-    if (!seed || seed === undefined) {
-        str = swapTwo(str, str.length - 2, str.length - 1);
-        arr.push(str);
-    }
-
-    return arr;
-}
-
-function swapTwo(str, index1, index2) {
-    let arr = str.split("");
-    // console.log("PRE-SWAP ARRAY: " + arr);
-    let x = arr[index1];
-    //console.log(x);
-    let y = arr[index2];
-    //console.log(y);
-    arr[index1] = y;
-    arr[index2] = x;
-    //console.log("POST-SWAP ARRAY: " + arr);
-    return arr.join("");
-}
-
-function factorial(num) {
-    // only positive numbers
-    if (num < 0) {
-        return -1;
-    }
-
-    if (num === 0) {
-        return 1;
-    } else {
-        return (num * factorial(num - 1));
+    // recursive permutation generator
+    function permutate(chars) {
+        if (chars.length === 0) permutations.push(nextWord.join(''));
+        for (let i = 0; i < chars.length; i++) {
+            chars.push(chars.shift()); //rotate the characters
+            nextWord.push(chars[0]); //use the first char in the array
+            permutate(chars.slice(1)); //Recurse: array-less-one-char
+            nextWord.pop(); //clear for nextWord (multiple pops)
+        }
     }
 }
 
 export {
-    permAlone,
-    swapTwo,
-    swapSet
+    permAlone
 };
