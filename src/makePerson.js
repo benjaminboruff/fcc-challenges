d7fd27288427e4106c7bd8f8f2f362c8a1dc0852
benjaminboruff <ben@boruff.me>
2017-01-07 12:42:51 -0500

    makePerson update

--- src/makePerson.js
+++ src/makePerson.js
@@ -1,12 +1,40 @@
 /* jshint esversion: 6 */
 class Person {
-  constructor(firstAndLast) {
-    let nameArr = firstAndLast.split(" ");
-    this.firstName = nameArr[0];
-    this.lastName = nameArr[1];
-  }
+    constructor(firstAndLast) {
+        this.fullName = firstAndLast;
+        let nameArr = this.fullName.split(" ");
+        this.firstName = nameArr[0];
+        this.lastName = nameArr[1];
+    }
 
-  
+    getFirstName() {
+        return this.firstName;
+    }
+
+    getLastName() {
+        return this.lastName;
+    }
+
+    getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    setFirstName(str) {
+        if (typeof str === 'string') this.firstName = str;
+    }
+
+    setLastName(str) {
+        if (typeof str === 'string') this.lastName = str;
+    }
+
+    setFullName(str) {
+        if (typeof str === 'string') {
+            this.fullName = str;
+            let nameArr = this.fullName.split(" ");
+            this.firstName = nameArr[0];
+            this.lastName = nameArr[1];
+        }
+    }
 
 }
 
