03358c7dce4084758c29ba60b88521545e65f9ba
benjaminboruff <ben@boruff.me>
2017-01-08 16:03:43 -0500

    pairwise update

--- src/pairwise.js
+++ src/pairwise.js
@@ -6,7 +6,7 @@ function pairwise(arr, arg) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] + arr[j] === arg) {
-                // if indices match, don't reuse them in further iterations
+                // if sum of indices match arg, don't reuse them in further iterations
                 if (!usedIndices.includes(i) && !usedIndices.includes(j)) {
                     usedIndices.push(i, j);
                     pairs.push([i, j]);
